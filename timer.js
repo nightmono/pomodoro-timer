ab7b672bc2518e3635139f485f5e9cca5687cfe5
nightmono <163682003+nightmono@users.noreply.github.com>
2024-05-18 22:44:13 +0100

    Make button toggle countdown

--- timer.js
+++ timer.js
@@ -4,12 +4,33 @@ var timeLeft = 25 * 60;
 function countdown() {
     timeLeft -= 1;
 
-    var timerText = document.getElementById('timer-text');
+    var timerText = document.getElementById("timer-text");
 
     var minutes = parseInt(timeLeft / 60);
-    var seconds = timeLeft % 60;
+    var seconds = String(timeLeft % 60).padStart(2, "0");
 
     timerText.textContent = `${minutes}:${seconds}`
 }
 
-setInterval(countdown, 1000);
+function startCountdown() {
+    countdownIntervalID = setInterval(countdown, 1000);
+
+    var startButton = document.getElementById("start-button");
+    startButton.textContent = "STOP";
+    startButton.style.boxShadow = "none";
+    startButton.style.marginTop = "14px";
+    startButton.style.marginBottom = "0";
+    startButton.onclick = stopCountdown;
+
+}
+
+function stopCountdown() {
+    clearInterval(countdownIntervalID);
+
+    var startButton = document.getElementById("start-button");
+    startButton.textContent = "START";
+    startButton.style.boxShadow = "rgb(235, 235, 235) 0px 6px 0px";
+    startButton.style.marginTop = "8px";
+    startButton.style.marginBottom = "6px";
+    startButton.onclick = startCountdown;
+}
